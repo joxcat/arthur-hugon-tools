7ddd462718c005baeba45d35e3a3a6cbebcce003
joxcat <contact@planchon.xyz>
2019-07-15 16:41:19 +0200

    Rewrite w/o &str

--- src/simple_db/mod.rs
+++ src/simple_db/mod.rs
@@ -5,13 +5,9 @@ use std::io::{Write, Read};
 use base64::{encode, decode};
 use std::collections::HashMap;
 
-pub struct SimpleDB<'a> {
-    pub inner: Mutex<SimpleDBInner<'a>>
-}
-
 #[derive(Debug)]
-pub struct SimpleDBInner<'a> {
-    pub path: &'a str
+pub struct SimpleDB {
+    pub inner: Mutex<String>
 }
 
 macro_rules! err {
@@ -24,19 +20,20 @@ macro_rules! string_from_bytes {
 
 const DELIM: char = '@';
 
-impl<'a> SimpleDB<'a> {
-    pub fn init(path: &'a str) -> Result<Self, SimpleDBError> {
-        if !Path::new(path).is_file() {
+impl SimpleDB {
+    pub fn init(_path: &str) -> Result<Self, SimpleDBError> {
+        let path = Path::new(_path);
+        if !path.is_file() {
             File::create(path).map_err(err!("Cannot create file"))?;
         }
-        Ok(Self { inner: Mutex::new(SimpleDBInner { path }) })
+        Ok(Self { inner: Mutex::new(_path.to_owned()) })
     }
     pub fn add(&self, key: &str, val: &str) -> Result<(), SimpleDBError> {
         if self.get(&(*key))?.is_none() {
             let locked = self.inner.lock().unwrap();
             let mut f = std::fs::OpenOptions::new()
                 .append(true)
-                .open(&locked.path)
+                .open(&Path::new(locked.as_str()))
                 .map_err(err!("Cannot open file"))?;
             f.write(format!("{}{}{}\n",
                             encode(key.as_bytes()),
@@ -48,7 +45,7 @@ impl<'a> SimpleDB<'a> {
     }
     pub fn get(&self, key: &str) -> Result<Option<String>, SimpleDBError> {
         let locked = self.inner.lock().unwrap();
-        let mut f = File::open(locked.path).map_err(err!("Cannot open file"))?;
+        let mut f = File::open(Path::new(locked.as_str())).map_err(err!("Cannot open file"))?;
         let mut file = String::new();
         f.read_to_string(&mut file).map_err(err!("Cannot read file"))?;
         let content: HashMap<String,String> = file.split('\n').map(|line: &str| {
@@ -63,7 +60,7 @@ impl<'a> SimpleDB<'a> {
     }
     pub fn remove(&self, key: &str) -> Result<(), SimpleDBError> {
         let locked = self.inner.lock().unwrap();
-        let mut f = File::open(locked.path).map_err(err!("Cannot open file"))?;
+        let mut f = File::open(Path::new(locked.as_str())).map_err(err!("Cannot open file"))?;
         let mut file = String::new();
         f.read_to_string(&mut file).map_err(err!("Cannot read file"))?;
         f.set_len(0).map_err(err!("Cannot clean file"))?;
@@ -83,7 +80,7 @@ impl<'a> SimpleDB<'a> {
     }
     pub fn update(&self, key: &str, new_val: &str) -> Result<(), SimpleDBError> {
         let locked = self.inner.lock().unwrap();
-        let mut f = File::open(locked.path).map_err(err!("Cannot open file"))?;
+        let mut f = File::open(Path::new(locked.as_str())).map_err(err!("Cannot open file"))?;
         let mut file = String::new();
         f.read_to_string(&mut file).map_err(err!("Cannot read file"))?;
         f.set_len(0).map_err(err!("Cannot clean file"))?;
@@ -105,7 +102,7 @@ impl<'a> SimpleDB<'a> {
     }
     pub fn get_all(&self) -> Result<HashMap<String,String>, SimpleDBError> {
         let locked = self.inner.lock().unwrap();
-        let mut f = File::open(locked.path).map_err(err!("Cannot open file"))?;
+        let mut f = File::open(Path::new(locked.as_str())).map_err(err!("Cannot open file"))?;
         let mut file = String::new();
         f.read_to_string(&mut file).map_err(err!("Cannot read file"))?;
         let content: HashMap<String,String> = file.split('\n').map(|line: &str| {
